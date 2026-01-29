193133260511161835db86bb5c9151d80b815c81
Vishal <vishal.200087@gmail.com>
2026-01-29 15:32:14 +0530

    Development Started

--- productlist/src/main/java/com/vishalspring/product/controller/ProductController.java
+++ productlist/src/main/java/com/vishalspring/product/controller/ProductController.java
@@ -1,14 +1,24 @@
 package com.vishalspring.product.controller;
 
+import com.vishalspring.product.dto.ProductDTO;
+import com.vishalspring.product.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/products")
+@AllArgsConstructor
 public class ProductController {
 
+    private ProductService productService;
     // getProduct
     // createProduct
+    public ProductDTO createProduct(@RequestBody ProductDTO productDTO){
+       return productService.createProduct(productDTO);
+
+    }
     // update product
     // get product by id
     // delete product
--- productlist/src/main/java/com/vishalspring/product/service/ProductService.java
+++ productlist/src/main/java/com/vishalspring/product/service/ProductService.java
@@ -1,9 +1,30 @@
 package com.vishalspring.product.service;
 
+import com.vishalspring.product.dto.ProductDTO;
+import com.vishalspring.product.entity.Category;
+import com.vishalspring.product.entity.Product;
+import com.vishalspring.product.mapper.ProductMapper;
+import com.vishalspring.product.repository.CategoryRepository;
+import com.vishalspring.product.repository.ProductRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class ProductService {
 
+    private ProductRepository productRepository;
+    private CategoryRepository categoryRepository;
+
+    public ProductDTO createProduct(ProductDTO productDTO){
+
+        Category category = categoryRepository.findById(productDTO.getCategoryId()).orElseThrow(()-> new RuntimeException("Category not found!"));
+         // DTO -> entity
+        Product product = ProductMapper.toProductEntity(productDTO, category);
+        product = productRepository.save(product);
+
+        // entity to DTO
+        return ProductMapper.toProductDTO(product);
+    }
 
 }
